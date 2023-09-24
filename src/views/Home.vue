7b47599cbf4026eeea08fd164ea6a41e4ea81467
Prett <41442866+jesspoex@users.noreply.github.com>
2023-09-24 12:50:50 +0700

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,7 +7,7 @@
         <h1
           class="text-primary display-4 font-weight-bolder d-none d-md-block"
         >
-        <img src="22.png" alt="logo" style="width:100px;height:100px;margin: 12px;" onContextMenu="return false;">
+        <img src="3.png" alt="logo" style="width:100px;height:100px;margin: 12px;" onContextMenu="return false;">
 
           jesspoex | EXPLORER<small class="flow-left"></small>
         </h1>
