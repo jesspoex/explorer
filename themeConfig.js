436e9e309699bd953acd1a5538474416dfb27c0f
Prett <41442866+jesspoex@users.noreply.github.com>
2023-09-24 15:08:34 +0700

    Update themeConfig.js

--- themeConfig.js
+++ themeConfig.js
@@ -11,7 +11,7 @@ export const $themeConfig = {
   app: {
     appName: 'EXPLORER', // Will update name in navigation menu (Branding)
     // eslint-disable-next-line global-require
-    appLogoImage: '/logo.svg', // Will update logo in navigation menu (Branding)
+    appLogoImage: '/1.svg', // Will update logo in navigation menu (Branding)
   },
   layout: {
     isRTL: false,
